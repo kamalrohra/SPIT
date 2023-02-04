2bcf6e9fc1b9da5db562e26a0547056f87d6b1dc
Swapnil Jha <59622008+Swapnil0506@users.noreply.github.com>
2023-02-04 21:04:52 +0530

    Frontend new changes

--- web/frontend/ppv/src/App.js
+++ web/frontend/ppv/src/App.js
@@ -3,10 +3,15 @@ import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Videos from './components/Videos';
 import Player from './components/Player';
+import PlayerPage from './components/PlayerPage';
+import Home from './components/Home';
+import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+import Profile from './components/profile';
 import Register from './components/Register';
+
 function App() {
   return (
-    <Router>
+    // <Router>
     <div className="App">
      {/* <ul className="App-header">
        <li>
@@ -17,12 +22,14 @@ function App() {
        </li>
      </ul> */}
      
-    <Routes>
+    {/* <Routes>
           <Route exact path='/' element={< Home />}></Route>
           <Route exact path='/video' element={< PlayerPage />}></Route>
-   </Routes>
-   </div>
-</Router>
+    </Routes> */}
+    {/* <Profile/> */}
+    <Register/>
+    </div>
+    // </Router>
   );
 }
 
