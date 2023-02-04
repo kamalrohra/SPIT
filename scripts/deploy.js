723d01be2ff7bb72192283feff3c24afb47daccf
pathikg <pathikghugare13@gmail.com>
2023-02-04 20:25:14 +0530

    Fixed bugs

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,16 @@
 async function main() {
-  const contract = await ethers.getContractFactory("medBlocks");
-  const medBlocksContract = await contract.deploy();
-  await medBlocksContract.deployed();
-  return medBlocksContract;
+  const contract = await ethers.getContractFactory("DOTT");
+  const DOTTContract = await contract.deploy();
+  await DOTTContract.deployed();
+  return DOTTContract;
 }
 
 main()
   .then(async (contract) => {
-    console.log("medBlocksContract deployed at: ", contract.address);
+    console.log("DOTTContract deployed at: ", contract.address);
   })
   .catch((error) => {
-    console.error("failed to deploy medBlocksContract", error);
+    console.error("failed to deploy DOTTContract", error);
   });
+
+//0x2970136dBA2d7F99ACEe5610a6338512Ae23bAF1
