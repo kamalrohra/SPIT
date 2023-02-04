e547c1a98122ca96a2bd26047cebe95df0e27f0b
Swapnil Jha <59622008+Swapnil0506@users.noreply.github.com>
2023-02-04 12:58:26 +0530

    Initial React App

---
+++ web/frontend/ppv/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import VideoDetail from './components/VideoDetail';
+
+function App() {
+  return (
+    <div className="App">
+      <VideoDetail/>
+
+
+
+    </div>
+  );
+}
+
+export default App;
