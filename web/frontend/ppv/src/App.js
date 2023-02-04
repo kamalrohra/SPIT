82f31666784a3714320a7cecc567b52d56b292a0
Swapnil Jha <59622008+Swapnil0506@users.noreply.github.com>
2023-02-04 18:09:40 +0530

    Users log page frontend

--- web/frontend/ppv/src/App.js
+++ web/frontend/ppv/src/App.js
@@ -4,15 +4,19 @@ import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Videos from './components/Videos';
 import DragDrop from './components/DragDrop';
+import Analytics from './components/Analytics';
+
 function App() {
   return (
     <div className="App">
       <Header/>
       <br/>
-      <div className="main-display" style={{"display" : "flex"}}>
+      {/* <div className="main-display" style={{"display" : "flex"}}>
       <Sidebar/>
        <Videos/>
-      </div>
+      </div> */}
+      <Analytics/>
+      {/* <Analytics/> */}
     </div>
   );
 }
